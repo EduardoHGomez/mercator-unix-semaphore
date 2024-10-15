684ebbfb7adb1e26a73dc6084f8be719d44da952
root <root@Edu>
2024-10-14 23:41:23 -0600

    last changes

--- mercator.c
+++ mercator.c
@@ -1,10 +1,9 @@
 /*           
-        Para compilar incluir la librería  m (matemáticas)
+        Para compilar incluir la librería m (matemáticas)
 
         Ejemplo:
-            gcc -o mercator mercator.c  -lm
+            gcc -o mercator mercator.c -lm -pthread
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,77 +11,127 @@
 #include <sys/time.h>
 #include <sys/wait.h>
 #include <sys/shm.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+// Semaforos a utilizar
+#define SEM_MASTER "/sem_master" // De master a slaves
+#define SEM_SLAVE "/sem_slave" // Semaforo de slaves cuando terminan
+#define MUTEX_SEM "/mutex_sem" // Importante: Empleo de exlusividad mutua
 
 #define NPROCS 4
 #define SERIES_MEMBER_COUNT 200000
 
 typedef struct { 
     double sums[NPROCS];
-    int proc_count;
-    int start_all;
     double x_val;
     double res;
+
+    // Sin utilizar
+    int proc_count;
+    int start_all;
 } SHARED;
 
 SHARED *shared;
 
+// Calcular cada termino n, entrega el numerador
 double get_member(int n, double x)
 {
     int i;
     double numerator = 1;
 
-    for(i=0; i<n; i++ )
-        numerator = numerator*x;
+    for(i = 0; i < n; i++)
+        numerator *= x;
 
     if (n % 2 == 0)
-        return ( - numerator / n );
+        return (-numerator / n);
     else
-        return numerator/n;
+        return (numerator / n);
 }
 
+// Proceso esclavo
 void proc(int proc_num)
 {
     int i;
 
-    // Espera a que master_proc ponga la bandera en 1 para iniciar
-    while(!(shared->start_all));
+    // Abrir semaforos
+    // Son locales, para referenciar cada una a su semaforo Global nombrado
+    sem_t *sem_master = sem_open(SEM_MASTER, 0);
+    sem_t *sem_slave = sem_open(SEM_SLAVE, 0);
+    sem_t *mutex_sem = sem_open(MUTEX_SEM, 0);
 
-    // Cada proceso realiza el cálculo de los términos que le tocan
-    shared->sums[proc_num] = 0;
-    for(i=proc_num; i<SERIES_MEMBER_COUNT;i+=NPROCS)
-        shared->sums[proc_num] += get_member(i+1, shared->x_val);
+    // Esperar a que inicie el master, reemplazando la linea
+    // while(!(shared->start_all));
+    sem_wait(sem_master); 
 
-    // Incrementa la variable proc_count que es la cantidad de procesos que terminaron su cálculo
-    shared->proc_count++;
+    // Seccion critica,
+    double suma_proceso = 0.0;
+    for(i = proc_num; i < SERIES_MEMBER_COUNT; i += NPROCS)
+        suma_proceso += get_member(i + 1, shared->x_val);
+
+    sem_wait(mutex_sem); // Semaforo EXCLUSIVIDAD 
+    shared->sums[proc_num] = suma_proceso;
+
+    sem_post(mutex_sem); // Compltar Exclusividad mutua
+    sem_post(sem_slave); // SIGNAL de esclavos
+
+    // Cerrar los semaforos
+    sem_close(sem_master);
+    sem_close(sem_slave);
+    sem_close(mutex_sem);
 
     exit(0);
 }
 
+// Funcion maestra
 void master_proc()
 {
     int i;
 
+    // Abrir los semaforos
+    sem_t *sem_master = sem_open(SEM_MASTER, 0);
+    sem_t *sem_slave = sem_open(SEM_SLAVE, 0);
+    sem_t *mutex_sem = sem_open(MUTEX_SEM, 0);
+
     // Obtener el valor de x desde el archivo entrada.txt
-    FILE *fp = fopen("entrada.txt","r");
-    if(fp==NULL)
+    FILE *fp = fopen("entrada.txt", "r");
+    if (fp == NULL) {
         exit(1);
-    
-    fscanf(fp,"%lf",&shared->x_val);
+    }
+
+    fscanf(fp, "%lf", &shared->x_val);
     fclose(fp);
 
-    // el master_proc pone una bandera para que los procesos inicien con el cálculo
-    shared->start_all=1;
+    // SIGNAL para que los esclavos inicien con los procesos
+    // POR ESTA RAZON comienza en 0 el semaforo, incrementa por NPROCS
+    for (i = 0; i < NPROCS; i++)
+        sem_post(sem_master);
 
-    // Espera a que todos los procesos terminen su cálculo
-    while (shared->proc_count != NPROCS) {}  
+    // Espera al signal para que se completen los semaforos
+    for (i = 0; i < NPROCS; i++)
+        sem_wait(sem_slave);
 
-    // Una vez que todos terminan, suma el total de cada uno
+    // Semaforo mutex para guardar el resultado
+    sem_wait(mutex_sem);
     shared->res = 0;
-    for(i=0; i<NPROCS; i++)
-        shared->res += shared->sums[i];
+    for (i = 0; i < NPROCS; i++)
+        shared->res += shared->sums[i]; // Sumar resultados
+    sem_post(mutex_sem); // Signal cuando termine de guardar en el semaforo binario
+
+    // Cerrar los semaforos
+    sem_close(sem_master);
+    sem_close(sem_slave);
+    sem_close(mutex_sem);
 
     exit(0);
 }
+
+
+sem_t *s_master;      
+sem_t *s_slave;        
+sem_t *s_exmut; 
+
 
 int main()
 {
@@ -98,30 +147,46 @@ int main()
     int shmid;
     int status;
 
-    // Solicita y conecta la memoria compartida
+   // Solicita y conecta la memoria compartida
     shmid = shmget(0x1234,sizeof(SHARED),0666|IPC_CREAT);
     shared = shmat(shmid,NULL,0);
  
     // inicializa las variables en memoria compartida
-    shared->proc_count = 0;
-    shared->start_all = 0;
- 
-    gettimeofday(&ts, NULL);
-    start_ts = ts.tv_sec; // Tiempo inicial
+    // shared->proc_count = 0;
+    // shared->start_all = 0;
+    for(i = 0; i < NPROCS; i++)
+        shared->sums[i] = 0.0;
 
-    for(i=0; i<NPROCS;i++)
+
+    // ------------------ Implementacion semaforos ----------------
+
+    // Eliminar los semaforos si existen
+    sem_unlink(SEM_MASTER);
+    sem_unlink(SEM_SLAVE);
+    sem_unlink(MUTEX_SEM);
+
+    // Abrir los semaforos con los permisos
+    s_master = sem_open(SEM_MASTER, O_CREAT | O_EXCL, S_IRUSR, 0);
+    s_slave = sem_open(SEM_SLAVE, O_CREAT | O_EXCL, S_IRUSR, 0);
+    s_exmut = sem_open(MUTEX_SEM, O_CREAT | O_EXCL, S_IRUSR, 1); // Mutex initialized to 1
+
+    gettimeofday(&ts, NULL);
+    start_ts = ts.tv_sec;
+
+    // Procesos esclavos TODO: Implementar proc(i) dado los semaforos
+    for(i = 0; i < NPROCS; i++)
     {
         p = fork();
-        if(p==0)
+        if(p == 0)
             proc(i);
     }
 
     p = fork();
-    if(p==0)
+    if(p == 0)
         master_proc();
 
-    printf("El recuento de ln(1 + x) miembros de la serie de Mercator es %d\n",SERIES_MEMBER_COUNT);
-    
+    printf("El recuento de ln(1 + x) miembros de la serie de Mercator es %d\n", SERIES_MEMBER_COUNT);
+
     for(int i=0;i<NPROCS+1;i++)
     {
         wait(&status);
@@ -131,16 +196,26 @@ int main()
             break;
         }
     }
-       
+
     gettimeofday(&ts, NULL);
-    stop_ts = ts.tv_sec; // Tiempo final
+    stop_ts = ts.tv_sec;
     elapsed_time = stop_ts - start_ts;
-    
+
     printf("Tiempo = %lld segundos\n", elapsed_time);
     printf("El resultado es %10.8f\n", shared->res);
     printf("Llamando a la función ln(1 + %f) = %10.8f\n",shared->x_val, log(1+shared->x_val));
 
-    // Desconecta y elimnina la memoria compartida
+    // Eliminar y desconectar semaforos
+    sem_close(s_master);
+    sem_close(s_slave);
+    sem_close(s_exmut);
+    sem_unlink(SEM_MASTER);
+    sem_unlink(SEM_SLAVE);
+    sem_unlink(MUTEX_SEM);
+
+    // Desconecta y elimina la memoria compartida
     shmdt(shared);
-    shmctl(shmid,IPC_RMID,NULL);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
 }
